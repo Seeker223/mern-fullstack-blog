714cd14ee9b13a96b7751a51aeaa519c94e19132
Seeker223 <baduntobi2024@gmail.com>
2024-05-13 15:14:16 +0100

    updated IndexPage

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,17 @@
 import './App.css';
 import Header from './Header';
+import Layout from './Layout';
 import Post from './Post';
 import { Route, Routes } from "react-router-dom";
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <Routes>
-      <Route index element={
-        <main>
-          <Header />
-          <Post />
-          <Post />
-          <Post />
-        </main>
-      } />
-      <Route path={"/login"} element={
-        <div>Login</div>
-      } />
+      <Route path='/' element={<Layout />}/>
+      <Route index element={ <IndexPage /> } />
+      <Route path="/login" element={<LoginPage />} />
       
     </Routes>
 
--- client/src/Header.js
+++ client/src/Header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Header() {
     return(
         <header>
-        <a href="" className="logo">MyBLog</a>
+        <Link to="/" className="logo">MyBLog</Link>
         <nav>
         <Link to="/login">Login</Link>
         <Link to="/register">Register</Link>
